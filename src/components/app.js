1deaaff520a12cb4e6734856e73f170d79f8bd03
sophiajiang <sophiajxw@gmail.com>
2016-07-28 15:10:46 -0400

    commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+
 
 // example class based component (smart component)
 class App extends Component {
@@ -6,8 +9,60 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      google: false,
+      googleName: '',
+      googleEmail: '',
+      googleImage: '',
     };
+
+    this.onFacebookLogin = this.onFacebookLogin.bind(this);
+    this.onGoogleLogin = this.onGoogleLogin.bind(this);
+    this.renderGoogle = this.renderGoogle.bind(this);
+  }
+
+  onGoogleLogin(response) {
+    if (response) {
+      const user = response.getBasicProfile();
+      this.setState({
+        google: true,
+        googleName: user.getName(),
+        googleEmail: user.getEmail(),
+        googleImage: user.getImageUrl(),
+      });
+    }
+  }
+
+  onFacebookLogin(response) {
+    if (response.status !== 'not_authorized' && response.status !== 'unknown') {
+      document.getElementById('facebook').innerHTML = `Welcome, ${response.name}! `;
+      document.getElementById('facebook').innerHTML += `<img src="${response.picture.data.url}"/>`;
+      document.getElementById('facebook').style = 'display: block';
+    } else {
+      document.getElementById('facebook').style = 'display: block';
+    }
+  }
+
+  renderGoogle() {
+    if (this.state.google) {
+      return (
+        <div>
+          <ul>
+            <li>Thanks for logging in! </li>
+            <li>Your Name: {this.state.googleName}</li>
+            <li>Your Email: {this.state.googleEmail}</li>
+            <li>Your Photo: <img src={this.state.googleImage} alt="N/A" /></li>
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <GoogleLogin
+          clientId="516853721256-jvunvdick45jec6jag1eem0cg9vkleu2.apps.googleusercontent.com"
+          buttonText="LOGIN WITH GOOGLE"
+          callback={this.onGoogleLogin}
+        />
+      );
+    }
   }
 
 
@@ -15,6 +70,17 @@ class App extends Component {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
       <div id="login">
+        <div id="facebook">
+          <FacebookLogin
+            appId="1153445964701726"
+            autoLoad
+            fields="name,email,picture.type(large)"
+            callback={this.onFacebookLogin}
+          />
+        </div>
+        <div id="google">
+          {this.renderGoogle()}
+        </div>
       </div>
     );
   }
